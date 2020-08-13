59198becd3e076be007cd55ee16305ea04637ea9
Chubbray <chubbray1@gmail.com>
2020-08-13 15:48:15 -0600

    updated server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const Sequelize = require("sequelize");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const path = require('path');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -24,3 +26,10 @@ sequelize.sync({ force: true }).then(function () {
 });
 
 module.exports = sequelize;
+
+app.use('/', express.static(__dirname + '/Views')); //translate static file requests from /log into /docs for showing documentation site.
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Views/index.html'));
+  });
+
